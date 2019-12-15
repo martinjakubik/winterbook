e8ec17d09165e04b7e814cbfbb7f30e52ec385b4
martin jakubik <martin.jakubik@gmail.com>
2019-12-15 20:22:03 +0100

    correctly cycled through the rainbow

--- learnjs/ex/myprogramme.js
+++ learnjs/ex/myprogramme.js
@@ -1,17 +1,60 @@
 let colorlist=['#f849cc','red','#f48720','yellow','#0cc656','#37a6eb','#940bda'];
 
-let move = 1;
+let normalizeColor = function (color) {
+
+    let normalizedColor = color;
+    let temporaryElement = document.createElement('p');
+    temporaryElement.style = 'color:' + color;
+    normalizedColor = temporaryElement.style.color;
+
+    return normalizedColor;
+
+};
+
+let normalizeColors = function (colorlist) {
+
+    let normalizedColorlist = [];
+    colorlist.forEach(color => normalizedColorlist.push(normalizeColor(color)));
+
+    return normalizedColorlist;
+
+};
+
+let normalizedColorlist = normalizeColors(colorlist);
+
+let getColorOfParagraph = function (paragraph) {
+
+    let style = paragraph.style;
+    let color = style.color;
+
+    return color;
+
+};
+
+let getNextColor = function (color, colorlist) {
+
+    let positionOfColorInList = colorlist.indexOf(color);
+    let positionOfNextColor = positionOfColorInList - 1 < 0 ? colorlist.length - 1 : positionOfColorInList - 1;
+    let nextColor = colorlist[positionOfNextColor];
+
+    return nextColor;
+
+};
 
 let changeColors=function(event) {
     let x = 0;
     while (x <= 900){
+
         let paragraphId = 'p' + x;
         let paragraphe = document.getElementById(paragraphId);
-        paragraphe.style='color:' + colorlist[(x - move) % 7];
+        let color = getColorOfParagraph(paragraphe);
+        let nextColor = getNextColor(color, normalizedColorlist);
+        paragraphe.style='color:' + nextColor;
+
         x = x + 1;
     };
-    move = (move + 1) % 7;
-}
+
+};
 
 let x = 0;
 while (x <= 900) {
@@ -20,7 +63,7 @@ while (x <= 900) {
     let paragraphe= document.createElement('p');
 
     paragraphe.innerHTML ='hello,loly';
-    paragraphe.style='color:' + colorlist[x%7];
+    paragraphe.style='color:' + normalizedColorlist[x%7];
     paragraphe.onclick = changeColors;
     paragraphe.id = 'p' + x;
 
