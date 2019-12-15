f6998c10a0395f862f9ef3ee1ece84fdff4e18cf
martin jakubik <martin.jakubik@gmail.com>
2019-12-15 19:47:46 +0100

    changed colours

--- learnjs/ex/myprogramme.js
+++ learnjs/ex/myprogramme.js
@@ -1,12 +1,29 @@
-let x = 0;
 let colorlist=['#f849cc','red','#f48720','yellow','#0cc656','#37a6eb','#940bda'];
-while (x <= 900){
+
+let move = 1;
+
+let changeColors=function(event) {
+    let x = 0;
+    while (x <= 900){
+        let paragraphId = 'p' + x;
+        let paragraphe = document.getElementById(paragraphId);
+        paragraphe.style='color:' + colorlist[(x - move) % 7];
+        x = x + 1;
+    };
+    move = (move + 1) % 7;
+}
+
+let x = 0;
+while (x <= 900) {
     console.log(x + ' hello,loly');
+
     let paragraphe= document.createElement('p');
+
     paragraphe.innerHTML ='hello,loly';
     paragraphe.style='color:' + colorlist[x%7];
-    
+    paragraphe.onclick = changeColors;
+    paragraphe.id = 'p' + x;
+
     document.body.insertBefore(paragraphe,null);
     x = x + 1;
 };
-
