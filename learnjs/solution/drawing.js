3c7c5de837d5c3f2dbab4653015c10ef85b8d379
martin jakubik <martin.jakubik@gmail.com>
2020-01-04 21:43:29 +0100

    solution for jump function

--- learnjs/solution/drawing.js
+++ learnjs/solution/drawing.js
@@ -1,4 +1,8 @@
 
+let sleep = function (milliseconds) {
+    return new Promise(resolve => setTimeout(resolve, milliseconds));
+}
+
 let canvas= document.createElement('canvas');
 document.body.insertBefore(canvas, null);
 canvas.width = 800;
@@ -20,23 +24,37 @@ let redrawBackground = function (oContext) {
 redrawBackground(context);
 
 let globoimage = new Image();
-globoimage.src='globohappy.png';
-globoimage.onload=function(){
+globoimage.src = 'globohappy.png';
+globoimage.onload = function(){
     context.drawImage(globoimage, 15, 600, 100, 100);
-}
+};
 
-let sleep = function (milliseconds) {
-    return new Promise(resolve => setTimeout(resolve, milliseconds));
-}
+let jump = async function (oContext) {
+
+    redrawBackground(oContext);
+    oContext.drawImage(globoimage, 15, 450, 100, 100);
+ 
+    let iHeight = 600;
+    let iFrame = 0;
+    while (iFrame < 6) {
+
+        iHeight = 600 - Math.abs(Math.sin(iFrame / 2) * 150);
+        redrawBackground(oContext);
+        oContext.drawImage(globoimage, 15, iHeight, 100, 100);
+        
+        await sleep(100);
+        iFrame = iFrame + 1;
+        
+    }
+
+    iHeight = 600;
+    redrawBackground(oContext);
+    oContext.drawImage(globoimage, 15, iHeight, 100, 100);
+
+};
 
 document.addEventListener('mouseup', async () => {
 
-    redrawBackground(context);
-    context.drawImage(globoimage, 15, 450, 100, 100);
- 
-    await sleep(100);
+    jump(context);
 
-    redrawBackground(context);
-    context.drawImage(globoimage, 15, 600, 100, 100);
- 
 })
